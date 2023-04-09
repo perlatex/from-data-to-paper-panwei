1692232d960c0b160814d05644ba43269ce71dfa
perlatex <wangmj@sicnu.edu.cn>
2023-04-09 20:55:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# from-data-to-paper-panwei
+# 从数据到论文
+
+运行前需要安装以下宏包：
+
+`install.packages(c("tidyverse", "readxl", "psych", "officedown", "officer", "flextable", "bookdown", "gtsummary", "modelsummary", "ggstatsplot", "ragg", "marginaleffects", "cowplot", "rstatix"))`
---
+++ data/01_prepare.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(readxl)
+
+# 读取原始数据
+raw_data <- read_excel("data.xlsx")
+
+# 规范列名
+pairs <- 
+  read_excel("colnames.xlsx") %>% 
+  deframe()
+
+# 重命名
+df <- raw_data %>% rename_with(.fn = ~pairs)
+
+glimpse(df)
+
+# 保存，方便后面使用
+df %>% 
+  select(-time_submit, -time_using, -source, -source_detail, -ip_from) %>% 
+  write_rds("df.rds")
